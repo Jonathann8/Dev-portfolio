97ed8f452ee830a117a4ca7e9563313660205d2b
Jonathan Vieira <jtvieira30@gmail.com>
2025-07-26 21:31:00 -0300

    Update app.py

--- Python/Sistema de Cadastro/app.py
+++ Python/Sistema de Cadastro/app.py
@@ -1,3 +1,7 @@
 import streamlit as st
 
 st.title("Sistema de Cadastro")
+
+nome = st.text_input("Digite o nome do cliente:")
+endereco = st.text_input("Digite o endereço:")
+dt_nasc = st.date_input("Escolha a data de nascimento:")
