8658163b59a58881370e18b1e6fb205570c3a51f
Jonathan Vieira <jtvieira30@gmail.com>
2025-07-26 21:49:40 -0300

    Aplicação V1

---
+++ Python/Sistema de Cadastro/Aplicação V1.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+st.title("Sistema de Cadastro")
+
+nome = st.text_input("Digite o nome do cliente:")
+endereco = st.text_input("Digite o endereço:")
+dt_nasc = st.date_input("Escolha a data de nascimento:")
+tipo_cliente = st.selectbox("Selecione o tipo de cliente:", ["Pessoa Física", "Pessoa Jurídica"])
+
+
+cadastrar = st.button("Cadastrar Cliente")
+
+if cadastrar:
+    with open("clientes.csv", "a") as arquivo:
+        arquivo.write(f"{nome},{endereco},{dt_nasc},{tipo_cliente}\n")
+        st.success("Cliente cadastrado com sucesso!")
